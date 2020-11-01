2030498f83ee8b23a0e1f6e5cde70aabf054b99c
hgko1207 <khkkhk1207@gmail.com>
2020-11-01 22:07:27 +0900

    화면 추가

--- AreaLighting/src/main/java/net/woorisys/lighting/controller/CityController.java
+++ AreaLighting/src/main/java/net/woorisys/lighting/controller/CityController.java
@@ -1,7 +1,21 @@
 package net.woorisys.lighting.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import net.woorisys.lighting.domain.db.City;
+import net.woorisys.lighting.domain.param.SearchParam;
+import net.woorisys.lighting.service.CityService;
 
 /**
  * 도시 화면 관리 Controller
@@ -12,5 +26,92 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("city")
 public class CityController {
+	
+	@Autowired
+	private CityService cityService;
 
+	/**
+	 * 목록 화면
+	 * @param model
+	 */
+	@GetMapping("list")
+    public void list(Model model) {
+    }
+	
+	/**
+	 * 조회
+	 * @param param
+	 * @return
+	 */
+	@PostMapping("search")
+	@ResponseBody 
+	public ResponseEntity<?> search(@RequestBody SearchParam param) {
+		return new ResponseEntity<>(cityService.getList(), HttpStatus.OK);
+	}
+	
+	/**
+	 * 등록 화면
+	 * @param model
+	 */
+	@GetMapping("regist")
+    public void regist(Model model) {
+    }
+	
+	/**
+	 * 등록
+	 * @param city
+	 * @return
+	 */
+	@PostMapping("regist")
+	@ResponseBody 
+	public ResponseEntity<?> regist(City city) {
+		if (cityService.regist(city)) {
+			return new ResponseEntity<>(HttpStatus.OK);
+		}
+		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+	}
+	
+	/**
+	 * 수정 화면
+	 * @param model
+	 */
+	@GetMapping("update")
+    public void update(Model model) {
+    }
+	
+	/**
+	 * 수정
+	 * @param city
+	 * @return
+	 */
+	@PutMapping("update")
+	@ResponseBody 
+	public ResponseEntity<?> update(City city) {
+		if (cityService.update(city)) {
+			return new ResponseEntity<>(HttpStatus.OK);
+		}
+		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+	}
+	
+	/**
+	 * 삭제 화면
+	 * @param model
+	 */
+	@GetMapping("delete")
+    public void delete(Model model) {
+    }
+	
+	/**
+	 * 삭제
+	 * @param id
+	 * @return
+	 */
+	@DeleteMapping("delete")
+	@ResponseBody 
+	public ResponseEntity<?> delete(Integer id) {
+		if (cityService.delete(id)) {
+			return new ResponseEntity<>(HttpStatus.OK);
+		}
+		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+	}
 }
---
+++ AreaLighting/src/main/java/net/woorisys/lighting/domain/param/SearchParam.java
@@ -0,0 +1,9 @@
+package net.woorisys.lighting.domain.param;
+
+import lombok.Data;
+
+@Data
+public class SearchParam {
+
+	private String name;
+}
